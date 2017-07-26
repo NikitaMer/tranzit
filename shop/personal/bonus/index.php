e92d39c44f17c54c788cfab3eff22b09c107c16c
Admin <git@webgk.ru>
2017-07-26 10:16:04 +0300

    SUP-6584: Editing site for component

--- shop/personal/bonus/index.php
+++ shop/personal/bonus/index.php
@@ -2,14 +2,23 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Бонусы");
 ?><?$APPLICATION->IncludeComponent(
-	"logictim:bonus.history",
-	"",
-	Array(
-		"FIELDS" => array("ID","DATE","NAME","OPERATION_SUM","BALLANCE_BEFORE","BALLANCE_AFTER"),
-		"ORDER_LINK" => "Y",
+	"logictim:bonus.history", 
+	".default", 
+	array(
+		"FIELDS" => array(
+			0 => "ID",
+			1 => "DATE",
+			2 => "NAME",
+			3 => "OPERATION_SUM",
+			4 => "BALLANCE_BEFORE",
+			5 => "BALLANCE_AFTER",
+		),
+		"ORDER_LINK" => "N",
 		"ORDER_URL" => "/personal/order/",
 		"PAGE_NAVIG_LIST" => "30",
 		"PAGE_NAVIG_TEMP" => "arrows",
-		"SORT" => "DESC"
-	)
+		"SORT" => "DESC",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
