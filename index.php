6b5f73c3c7fcab6f89dda875f1629289937eb1d0
Admin <git@webgk.ru>
2017-08-28 15:30:51 +0300

    SUP-6862: change encoding

--- index.php
+++ index.php
@@ -18,132 +18,132 @@ $APPLICATION->SetTitle("«Транзит-Ойл» — оптово-рознич�
  <br>
  Мы являемся официальными дилерами следующих <a href="/okompanii/brendy/">торговых марок</a>:<br>
 <ul class="red">
-	<li>автомобильные масла «ЛУКОЙЛ», «MOTUL» (Франция);</li>
-	<li>
-	грузовые шины «Michelin» и «Kormoran» (Франция);</li>
-	<li>
-	антифризы «CoolStream»;</li>
-	<li>
-	автохимия «RUNWAY» (США-Россия), «Sapfire»;</li>
-	<li>
-	автоаксессуары «Cafa France»;</li>
-	<li>
-	все виды автомобильных фильтров «BigFilter» (Россия) и «MANN» (Германия);</li>
-	<li>
-	автоаксессуары «PHANTOM»;</li>
-	<li>запасные части «NGK» и «Finwhale».</li>
+    <li>автомобильные масла «ЛУКОЙЛ», «MOTUL» (Франция);</li>
+    <li>
+    грузовые шины «Michelin» и «Kormoran» (Франция);</li>
+    <li>
+    антифризы «CoolStream»;</li>
+    <li>
+    автохимия «RUNWAY» (США-Россия), «Sapfire»;</li>
+    <li>
+    автоаксессуары «Cafa France»;</li>
+    <li>
+    все виды автомобильных фильтров «BigFilter» (Россия) и «MANN» (Германия);</li>
+    <li>
+    автоаксессуары «PHANTOM»;</li>
+    <li>запасные части «NGK» и «Finwhale».</li>
 </ul>
  Мы работаем по самым современным стандартам: автоматизация процессов увеличивает скорость обработки заказов, а современный складской комплекс позволяет хранить 3-х месячный запас автотоваров и обеспечивать потребность клиента в полном объёме.<br>
  <br>
  <br>
 <div class="columns">
-	<div class="col col50 pd-20">
-		 <?$APPLICATION->IncludeComponent(
-	"bitrix:news.list",
-	"main_page_akcii",
-	Array(
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"ADD_SECTIONS_CHAIN" => "N",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "36000000",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "Y",
-		"DETAIL_URL" => "",
-		"DISPLAY_BOTTOM_PAGER" => "N",
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array(0=>"",1=>"",),
-		"FILTER_NAME" => "",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "8",
-		"IBLOCK_TYPE" => "events",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"NEWS_COUNT" => "3",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Новости",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array(0=>"",1=>"",),
-		"SET_BROWSER_TITLE" => "N",
-		"SET_META_DESCRIPTION" => "N",
-		"SET_META_KEYWORDS" => "N",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "N",
-		"SORT_BY1" => "ACTIVE_FROM",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC"
-	)
+    <div class="col col50 pd-20">
+         <?$APPLICATION->IncludeComponent(
+    "bitrix:news.list",
+    "main_page_akcii",
+    Array(
+        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+        "ADD_SECTIONS_CHAIN" => "N",
+        "AJAX_MODE" => "N",
+        "AJAX_OPTION_ADDITIONAL" => "",
+        "AJAX_OPTION_HISTORY" => "N",
+        "AJAX_OPTION_JUMP" => "N",
+        "AJAX_OPTION_STYLE" => "Y",
+        "CACHE_FILTER" => "N",
+        "CACHE_GROUPS" => "Y",
+        "CACHE_TIME" => "36000000",
+        "CACHE_TYPE" => "A",
+        "CHECK_DATES" => "Y",
+        "DETAIL_URL" => "",
+        "DISPLAY_BOTTOM_PAGER" => "N",
+        "DISPLAY_DATE" => "Y",
+        "DISPLAY_NAME" => "Y",
+        "DISPLAY_PICTURE" => "Y",
+        "DISPLAY_PREVIEW_TEXT" => "Y",
+        "DISPLAY_TOP_PAGER" => "N",
+        "FIELD_CODE" => array(0=>"",1=>"",),
+        "FILTER_NAME" => "",
+        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+        "IBLOCK_ID" => "8",
+        "IBLOCK_TYPE" => "events",
+        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+        "INCLUDE_SUBSECTIONS" => "Y",
+        "NEWS_COUNT" => "3",
+        "PAGER_DESC_NUMBERING" => "N",
+        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+        "PAGER_SHOW_ALL" => "N",
+        "PAGER_SHOW_ALWAYS" => "N",
+        "PAGER_TEMPLATE" => ".default",
+        "PAGER_TITLE" => "Новости",
+        "PARENT_SECTION" => "",
+        "PARENT_SECTION_CODE" => "",
+        "PREVIEW_TRUNCATE_LEN" => "",
+        "PROPERTY_CODE" => array(0=>"",1=>"",),
+        "SET_BROWSER_TITLE" => "N",
+        "SET_META_DESCRIPTION" => "N",
+        "SET_META_KEYWORDS" => "N",
+        "SET_STATUS_404" => "N",
+        "SET_TITLE" => "N",
+        "SORT_BY1" => "ACTIVE_FROM",
+        "SORT_BY2" => "SORT",
+        "SORT_ORDER1" => "DESC",
+        "SORT_ORDER2" => "ASC"
+    )
 );?>
-	</div>
-	<div class="col col50 pd-l44 bg">
-		 <?$APPLICATION->IncludeComponent(
-	"bitrix:news.list",
-	"main_page_news",
-	Array(
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"ADD_SECTIONS_CHAIN" => "N",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "36000000",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "Y",
-		"DETAIL_URL" => "",
-		"DISPLAY_BOTTOM_PAGER" => "N",
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array(0=>"",1=>"",),
-		"FILTER_NAME" => "",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "2",
-		"IBLOCK_TYPE" => "events",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"NEWS_COUNT" => "3",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "Y",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Новости",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array(0=>"",1=>"",),
-		"SET_BROWSER_TITLE" => "N",
-		"SET_META_DESCRIPTION" => "N",
-		"SET_META_KEYWORDS" => "N",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "N",
-		"SORT_BY1" => "ACTIVE_FROM",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC"
-	)
+    </div>
+    <div class="col col50 pd-l44 bg">
+         <?$APPLICATION->IncludeComponent(
+    "bitrix:news.list",
+    "main_page_news",
+    Array(
+        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+        "ADD_SECTIONS_CHAIN" => "N",
+        "AJAX_MODE" => "N",
+        "AJAX_OPTION_ADDITIONAL" => "",
+        "AJAX_OPTION_HISTORY" => "N",
+        "AJAX_OPTION_JUMP" => "N",
+        "AJAX_OPTION_STYLE" => "Y",
+        "CACHE_FILTER" => "N",
+        "CACHE_GROUPS" => "Y",
+        "CACHE_TIME" => "36000000",
+        "CACHE_TYPE" => "A",
+        "CHECK_DATES" => "Y",
+        "DETAIL_URL" => "",
+        "DISPLAY_BOTTOM_PAGER" => "N",
+        "DISPLAY_DATE" => "Y",
+        "DISPLAY_NAME" => "Y",
+        "DISPLAY_PICTURE" => "Y",
+        "DISPLAY_PREVIEW_TEXT" => "Y",
+        "DISPLAY_TOP_PAGER" => "N",
+        "FIELD_CODE" => array(0=>"",1=>"",),
+        "FILTER_NAME" => "",
+        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+        "IBLOCK_ID" => "2",
+        "IBLOCK_TYPE" => "events",
+        "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+        "INCLUDE_SUBSECTIONS" => "Y",
+        "NEWS_COUNT" => "3",
+        "PAGER_DESC_NUMBERING" => "N",
+        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+        "PAGER_SHOW_ALL" => "Y",
+        "PAGER_SHOW_ALWAYS" => "N",
+        "PAGER_TEMPLATE" => ".default",
+        "PAGER_TITLE" => "Новости",
+        "PARENT_SECTION" => "",
+        "PARENT_SECTION_CODE" => "",
+        "PREVIEW_TRUNCATE_LEN" => "",
+        "PROPERTY_CODE" => array(0=>"",1=>"",),
+        "SET_BROWSER_TITLE" => "N",
+        "SET_META_DESCRIPTION" => "N",
+        "SET_META_KEYWORDS" => "N",
+        "SET_STATUS_404" => "N",
+        "SET_TITLE" => "N",
+        "SORT_BY1" => "ACTIVE_FROM",
+        "SORT_BY2" => "SORT",
+        "SORT_ORDER1" => "DESC",
+        "SORT_ORDER2" => "ASC"
+    )
 );?>
-	</div>
+    </div>
 </div>
  <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- local/templates/tranzit-oil_v2/components/bitrix/catalog.section/deals/template.php
+++ local/templates/tranzit-oil_v2/components/bitrix/catalog.section/deals/template.php
@@ -16,75 +16,75 @@ $count = 0;
 if (!empty($arResult['ITEMS']))
 {
 
-	?>
-	<div class="catalog-specoffers">
-		<h3>Хиты продаж</h3>
+    ?>
+    <div class="catalog-specoffers">
+        <h3>Хиты продаж</h3>
 
-		<?
-		if ($arParams["DISPLAY_TOP_PAGER"])
-			echo $arResult["NAV_STRING"];
+        <?
+        if ($arParams["DISPLAY_TOP_PAGER"])
+            echo $arResult["NAV_STRING"];
 
-		?><ul class="catalog-specoffers-list"><?
+        ?><ul class="catalog-specoffers-list"><?
 
-		$strElementEdit = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_EDIT");
-		$strElementDelete = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_DELETE");
-		$arElementDeleteParams = array("CONFIRM" => GetMessage('CT_BCS_TPL_ELEMENT_DELETE_CONFIRM'));
+        $strElementEdit = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_EDIT");
+        $strElementDelete = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_DELETE");
+        $arElementDeleteParams = array("CONFIRM" => GetMessage('CT_BCS_TPL_ELEMENT_DELETE_CONFIRM'));
 
-		foreach ($arResult['ITEMS'] as $key => $arItem)
-		{
-			$count++;
+        foreach ($arResult['ITEMS'] as $key => $arItem)
+        {
+            $count++;
 
-			$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], $strElementEdit);
-			$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], $strElementDelete, $arElementDeleteParams);
-			$strMainID = $this->GetEditAreaId($arItem['ID']);
+            $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], $strElementEdit);
+            $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], $strElementDelete, $arElementDeleteParams);
+            $strMainID = $this->GetEditAreaId($arItem['ID']);
 
-			$productTitle = (
-				isset($arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'])&& $arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'] != ''
-				? $arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE']
-				: $arItem['NAME']
-				);
+            $productTitle = (
+                isset($arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'])&& $arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'] != ''
+                ? $arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE']
+                : $arItem['NAME']
+                );
 
-			$imgTitle = (
-				isset($arItem['IPROPERTY_VALUES']['ELEMENT_PREVIEW_PICTURE_FILE_TITLE']) && $arItem['IPROPERTY_VALUES']['ELEMENT_PREVIEW_PICTURE_FILE_TITLE'] != ''
-				? $arItem['IPROPERTY_VALUES']['ELEMENT_PREVIEW_PICTURE_FILE_TITLE']
-				: $arItem['NAME']
-				);
+            $imgTitle = (
+                isset($arItem['IPROPERTY_VALUES']['ELEMENT_PREVIEW_PICTURE_FILE_TITLE']) && $arItem['IPROPERTY_VALUES']['ELEMENT_PREVIEW_PICTURE_FILE_TITLE'] != ''
+                ? $arItem['IPROPERTY_VALUES']['ELEMENT_PREVIEW_PICTURE_FILE_TITLE']
+                : $arItem['NAME']
+                );
 
-			$pict = null;//$arItem['PREVIEW_PICTURE'];
+            $pict = null;//$arItem['PREVIEW_PICTURE'];
 
-			if(!$pict && is_array($arItem['DETAIL_PICTURE']))
-				$pict = $arItem['DETAIL_PICTURE'];
+            if(!$pict && is_array($arItem['DETAIL_PICTURE']))
+                $pict = $arItem['DETAIL_PICTURE'];
 
-			if(!$pict && is_array($arItem['PREVIEW_PICTURE']))
-				$pict = $arItem['PREVIEW_PICTURE'];
+            if(!$pict && is_array($arItem['PREVIEW_PICTURE']))
+                $pict = $arItem['PREVIEW_PICTURE'];
 
-			$img = CFile::ResizeImageGet($pict, array('width'=>138, 'height'=>138), BX_RESIZE_IMAGE_PROPORTIONAL, true);
-			?>
-			<li id="<? echo $strMainID; ?>">
-				<a href="<? echo $arItem['DETAIL_PAGE_URL']; ?>" class="image" style="background-image: url(<?=$img['src'];?>)" title="<?=$imgTitle;?>"></a>
-				<div class="desc">
-					<a href="<? echo $arItem['DETAIL_PAGE_URL']; ?>" class="catalog-name"><?=TruncateText($productTitle, 30);?></a>
-					<div class="price-block">
-					<?
-						if (!empty($arItem['MIN_PRICE']) && (!isset($arItem['OFFERS']) || empty($arItem['OFFERS'])))
-						{
-							echo $arItem['MIN_PRICE']['PRINT_DISCOUNT_VALUE'];
-						}
-						?>
-					</div>
-				</div>
-			</li>
-			<?if($count % 6 == 0):?>
-			</ul>
-			<ul class="catalog-specoffers-list catalog-items">
-			<?endif;?>
-			<?
-		}
-		?></ul><?
+            $img = CFile::ResizeImageGet($pict, array('width'=>138, 'height'=>138), BX_RESIZE_IMAGE_PROPORTIONAL, true);
+            ?>
+            <li id="<? echo $strMainID; ?>">
+                <a href="<? echo $arItem['DETAIL_PAGE_URL']; ?>" class="image" style="background-image: url(<?=$img['src'];?>)" title="<?=$imgTitle;?>"></a>
+                <div class="desc">
+                    <a href="<? echo $arItem['DETAIL_PAGE_URL']; ?>" class="catalog-name"><?=TruncateText($productTitle, 30);?></a>
+                    <div class="price-block">
+                    <?
+                        if (!empty($arItem['MIN_PRICE']) && (!isset($arItem['OFFERS']) || empty($arItem['OFFERS'])))
+                        {
+                            echo $arItem['MIN_PRICE']['PRINT_DISCOUNT_VALUE'];
+                        }
+                        ?>
+                    </div>
+                </div>
+            </li>
+            <?if($count % 6 == 0):?>
+            </ul>
+            <ul class="catalog-specoffers-list catalog-items">
+            <?endif;?>
+            <?
+        }
+        ?></ul><?
 
-	if ($arParams["DISPLAY_BOTTOM_PAGER"])
-		echo $arResult["NAV_STRING"];
+    if ($arParams["DISPLAY_BOTTOM_PAGER"])
+        echo $arResult["NAV_STRING"];
 
-	?></div><?
+    ?></div><?
 }
 ?>
