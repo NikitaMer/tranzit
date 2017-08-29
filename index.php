baed5eb15937a0e4d908fc06a302de4fb330d705
Admin <git@webgk.ru>
2017-08-29 15:53:30 +0300

    local change 29.08

--- index.php
+++ index.php
@@ -10,140 +10,143 @@ $APPLICATION->SetPageProperty("keywords", "автотовары, автохим�
 $APPLICATION->SetPageProperty("h1", "«Транзит-Ойл» — С нами Вы всегда в плюсе!");
 $APPLICATION->SetTitle("«Транзит-Ойл» — оптово-розничная продажа автотоваров");
 ?><a href="/avtotovary/avtomasla/lukoil/"><img width="701" alt="официальный дилер лукойл татарстан" src="/okompanii/Баннер Genesis 2017-3.jpg" height="395" title="официальный дилер лукойл татарстан"></a><br>
-<br>
-<img width="180" src="/okompanii/f0000.jpg" height="105" align="left" class="otstup-right">Уже более 13 лет наша компания специализируется на оптовых продажах автотоваров в Республике Татарстан. Мы работаем напрямую с производителями смазочных материалов, автохимии, автомобильных аккумуляторов, запчастей, грузовых шин. Являемся официальным дилером 16 торговых марок.<br>
+ <br>
+ <img width="180" src="/okompanii/f0000.jpg" height="105" align="left" class="otstup-right">Уже более 13 лет наша компания специализируется на оптовых продажах автотоваров в Республике Татарстан. Мы работаем напрямую с производителями смазочных материалов, автохимии, автомобильных аккумуляторов, запчастей, грузовых шин. Являемся официальным дилером 16 торговых марок.<br>
  <br>
 <h2>Наша деятельность представлена:</h2>
 <!--noindex--><a href="/avtotovary/"><img width="170" alt="автотовары оптом" src="/okompanii/r0005.jpg" height="215" title="автотовары оптом"></a><!--/noindex--> <a href="/retail-network/"><img width="170" alt="розничная продажа автотоваров" src="/okompanii/r0006.jpg" height="215" title="розничная продажа автотоваров"></a><a href="/shop/" rel="nofollow" target="_blank"><img width="170" alt="интернет-магазин автотоваров" src="/okompanii/r0007.jpg" height="215" title="интернет-магазин автотоваров"></a> <a href="/gruzoviye-shiny/"><img width="170" alt="грузовые шины, грузовой шиномонтаж" src="/okompanii/r0009.jpg" height="215" title="грузовые шины, грузовой шиномонтаж"></a><br>
  <br>
  Мы являемся официальными дилерами следующих <a href="/okompanii/brendy/">торговых марок</a>:<br>
 <ul class="red">
-    <li>автомобильные масла «ЛУКОЙЛ», «MOTUL» (Франция);</li>
-    <li>
-    грузовые шины «Michelin» и «Kormoran» (Франция);</li>
-    <li>
-    антифризы «CoolStream»;</li>
-    <li>
-    автохимия «RUNWAY» (США-Россия), «Sapfire»;</li>
-    <li>
-    автоаксессуары «Cafa France»;</li>
-    <li>
-    все виды автомобильных фильтров «BigFilter» (Россия) и «MANN» (Германия);</li>
-    <li>
-    автоаксессуары «PHANTOM»;</li>
-    <li>запасные части «NGK» и «Finwhale».</li>
+	<li>автомобильные масла «ЛУКОЙЛ», «MOTUL» (Франция);</li>
+	<li>
+	грузовые шины «Michelin» и «Kormoran» (Франция);</li>
+	<li>
+	антифризы «CoolStream»;</li>
+	<li>
+	автохимия «RUNWAY» (США-Россия), «Sapfire»;</li>
+	<li>
+	автоаксессуары «Cafa France»;</li>
+	<li>
+	все виды автомобильных фильтров «BigFilter» (Россия)&nbsp;и «MANN» (Германия);</li>
+	<li>
+	автоаксессуары «PHANTOM»;</li>
+	<li>запасные части&nbsp;«NGK»&nbsp;и «Finwhale».</li>
 </ul>
  Мы работаем по самым современным стандартам: автоматизация процессов увеличивает скорость обработки заказов, а современный складской комплекс позволяет хранить 3-х месячный запас автотоваров и обеспечивать потребность клиента в полном объёме.<br>
  <br>
+ <b>ПРЕЗЕНТАЦИЯ О КОМПАНИИ&nbsp;- <a href="/presentation_tranzit-oil_2017.pdf">СКАЧАТЬ</a> </b><br>
+ <br>
+ <br>
  <br>
 <div class="columns">
-    <div class="col col50 pd-20">
-         <?$APPLICATION->IncludeComponent(
-    "bitrix:news.list",
-    "main_page_akcii",
-    Array(
-        "ACTIVE_DATE_FORMAT" => "d.m.Y",
-        "ADD_SECTIONS_CHAIN" => "N",
-        "AJAX_MODE" => "N",
-        "AJAX_OPTION_ADDITIONAL" => "",
-        "AJAX_OPTION_HISTORY" => "N",
-        "AJAX_OPTION_JUMP" => "N",
-        "AJAX_OPTION_STYLE" => "Y",
-        "CACHE_FILTER" => "N",
-        "CACHE_GROUPS" => "Y",
-        "CACHE_TIME" => "36000000",
-        "CACHE_TYPE" => "A",
-        "CHECK_DATES" => "Y",
-        "DETAIL_URL" => "",
-        "DISPLAY_BOTTOM_PAGER" => "N",
-        "DISPLAY_DATE" => "Y",
-        "DISPLAY_NAME" => "Y",
-        "DISPLAY_PICTURE" => "Y",
-        "DISPLAY_PREVIEW_TEXT" => "Y",
-        "DISPLAY_TOP_PAGER" => "N",
-        "FIELD_CODE" => array(0=>"",1=>"",),
-        "FILTER_NAME" => "",
-        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
-        "IBLOCK_ID" => "8",
-        "IBLOCK_TYPE" => "events",
-        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-        "INCLUDE_SUBSECTIONS" => "Y",
-        "NEWS_COUNT" => "3",
-        "PAGER_DESC_NUMBERING" => "N",
-        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-        "PAGER_SHOW_ALL" => "N",
-        "PAGER_SHOW_ALWAYS" => "N",
-        "PAGER_TEMPLATE" => ".default",
-        "PAGER_TITLE" => "Новости",
-        "PARENT_SECTION" => "",
-        "PARENT_SECTION_CODE" => "",
-        "PREVIEW_TRUNCATE_LEN" => "",
-        "PROPERTY_CODE" => array(0=>"",1=>"",),
-        "SET_BROWSER_TITLE" => "N",
-        "SET_META_DESCRIPTION" => "N",
-        "SET_META_KEYWORDS" => "N",
-        "SET_STATUS_404" => "N",
-        "SET_TITLE" => "N",
-        "SORT_BY1" => "ACTIVE_FROM",
-        "SORT_BY2" => "SORT",
-        "SORT_ORDER1" => "DESC",
-        "SORT_ORDER2" => "ASC"
-    )
+	<div class="col col50 pd-20">
+		 <?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"main_page_akcii",
+	Array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "N",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(0=>"",1=>"",),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "8",
+		"IBLOCK_TYPE" => "events",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"NEWS_COUNT" => "3",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(0=>"",1=>"",),
+		"SET_BROWSER_TITLE" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC"
+	)
 );?>
-    </div>
-    <div class="col col50 pd-l44 bg">
-         <?$APPLICATION->IncludeComponent(
-    "bitrix:news.list",
-    "main_page_news",
-    Array(
-        "ACTIVE_DATE_FORMAT" => "d.m.Y",
-        "ADD_SECTIONS_CHAIN" => "N",
-        "AJAX_MODE" => "N",
-        "AJAX_OPTION_ADDITIONAL" => "",
-        "AJAX_OPTION_HISTORY" => "N",
-        "AJAX_OPTION_JUMP" => "N",
-        "AJAX_OPTION_STYLE" => "Y",
-        "CACHE_FILTER" => "N",
-        "CACHE_GROUPS" => "Y",
-        "CACHE_TIME" => "36000000",
-        "CACHE_TYPE" => "A",
-        "CHECK_DATES" => "Y",
-        "DETAIL_URL" => "",
-        "DISPLAY_BOTTOM_PAGER" => "N",
-        "DISPLAY_DATE" => "Y",
-        "DISPLAY_NAME" => "Y",
-        "DISPLAY_PICTURE" => "Y",
-        "DISPLAY_PREVIEW_TEXT" => "Y",
-        "DISPLAY_TOP_PAGER" => "N",
-        "FIELD_CODE" => array(0=>"",1=>"",),
-        "FILTER_NAME" => "",
-        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
-        "IBLOCK_ID" => "2",
-        "IBLOCK_TYPE" => "events",
-        "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-        "INCLUDE_SUBSECTIONS" => "Y",
-        "NEWS_COUNT" => "3",
-        "PAGER_DESC_NUMBERING" => "N",
-        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-        "PAGER_SHOW_ALL" => "Y",
-        "PAGER_SHOW_ALWAYS" => "N",
-        "PAGER_TEMPLATE" => ".default",
-        "PAGER_TITLE" => "Новости",
-        "PARENT_SECTION" => "",
-        "PARENT_SECTION_CODE" => "",
-        "PREVIEW_TRUNCATE_LEN" => "",
-        "PROPERTY_CODE" => array(0=>"",1=>"",),
-        "SET_BROWSER_TITLE" => "N",
-        "SET_META_DESCRIPTION" => "N",
-        "SET_META_KEYWORDS" => "N",
-        "SET_STATUS_404" => "N",
-        "SET_TITLE" => "N",
-        "SORT_BY1" => "ACTIVE_FROM",
-        "SORT_BY2" => "SORT",
-        "SORT_ORDER1" => "DESC",
-        "SORT_ORDER2" => "ASC"
-    )
+	</div>
+	<div class="col col50 pd-l44 bg">
+		 <?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"main_page_news",
+	Array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "N",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(0=>"",1=>"",),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "2",
+		"IBLOCK_TYPE" => "events",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"NEWS_COUNT" => "3",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "Y",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(0=>"",1=>"",),
+		"SET_BROWSER_TITLE" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC"
+	)
 );?>
-    </div>
+	</div>
 </div>
  <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
