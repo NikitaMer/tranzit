a5744c8b3031b8d130c7c1cbfdae7e40e7e5a03e
Nikita <mn@webgk.ru>
2017-10-23 16:09:45 +0300

    SUP-7080: change text in element page

--- local/templates/tranzit-oil_v2/components/bitrix/catalog.section/recomendation/template.php
+++ local/templates/tranzit-oil_v2/components/bitrix/catalog.section/recomendation/template.php
@@ -16,86 +16,86 @@ $CSiteController = SiteController::getEntity();
 
 if (!empty($arResult['ITEMS']))
 {
-	if ($arParams["DISPLAY_TOP_PAGER"])
-		echo $arResult["NAV_STRING"];
+    if ($arParams["DISPLAY_TOP_PAGER"])
+        echo $arResult["NAV_STRING"];
 
-	?>
-	<div class="title">С этим товаром покупают</div>
+    ?>
+    <div class="title">Рекомендуемые товары</div>
 
-	<ul class="catalog-specoffers-list"><?
+    <ul class="catalog-specoffers-list"><?
 
-	$strElementEdit = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_EDIT");
-	$strElementDelete = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_DELETE");
-	$arElementDeleteParams = array("CONFIRM" => GetMessage('CT_BCS_TPL_ELEMENT_DELETE_CONFIRM'));
+    $strElementEdit = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_EDIT");
+    $strElementDelete = CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_DELETE");
+    $arElementDeleteParams = array("CONFIRM" => GetMessage('CT_BCS_TPL_ELEMENT_DELETE_CONFIRM'));
 
-	foreach ($arResult['ITEMS'] as $key => $arItem)
-	{
-		$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], $strElementEdit);
-		$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], $strElementDelete, $arElementDeleteParams);
-		$strMainID = $this->GetEditAreaId($arItem['ID']);
+    foreach ($arResult['ITEMS'] as $key => $arItem)
+    {
+        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], $strElementEdit);
+        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], $strElementDelete, $arElementDeleteParams);
+        $strMainID = $this->GetEditAreaId($arItem['ID']);
 
-		$productTitle = (
-			isset($arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'])&& $arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'] != ''
-			? $arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE']
-			: $arItem['NAME']
-			);
-			
-		$imgTitle = (
-			isset($arItem['IPROPERTY_VALUES']['ELEMENT_PREVIEW_PICTURE_FILE_TITLE']) && $arItem['IPROPERTY_VALUES']['ELEMENT_PREVIEW_PICTURE_FILE_TITLE'] != ''
-			? $arItem['IPROPERTY_VALUES']['ELEMENT_PREVIEW_PICTURE_FILE_TITLE']
-			: $arItem['NAME']
-			);
+        $productTitle = (
+            isset($arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'])&& $arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE'] != ''
+            ? $arItem['IPROPERTY_VALUES']['ELEMENT_PAGE_TITLE']
+            : $arItem['NAME']
+            );
+            
+        $imgTitle = (
+            isset($arItem['IPROPERTY_VALUES']['ELEMENT_PREVIEW_PICTURE_FILE_TITLE']) && $arItem['IPROPERTY_VALUES']['ELEMENT_PREVIEW_PICTURE_FILE_TITLE'] != ''
+            ? $arItem['IPROPERTY_VALUES']['ELEMENT_PREVIEW_PICTURE_FILE_TITLE']
+            : $arItem['NAME']
+            );
 
-		$pict = null;//$arItem['PREVIEW_PICTURE'];
+        $pict = null;//$arItem['PREVIEW_PICTURE'];
 
 
-		if(!$pict && is_array($arItem['DETAIL_PICTURE']))
-			$pict = $arItem['DETAIL_PICTURE'];
+        if(!$pict && is_array($arItem['DETAIL_PICTURE']))
+            $pict = $arItem['DETAIL_PICTURE'];
 
-		if(!$pict && is_array($arItem['PREVIEW_PICTURE']))
-			$pict = $arItem['PREVIEW_PICTURE'];
+        if(!$pict && is_array($arItem['PREVIEW_PICTURE']))
+            $pict = $arItem['PREVIEW_PICTURE'];
 
         $image = CFile::ResizeImageGet($pict, array('width'=>82, 'height'=>82), BX_RESIZE_IMAGE_PROPORTIONAL, true);
-		?>
-		<li id="<? echo $strMainID; ?>">
+        ?>
+        <li id="<? echo $strMainID; ?>">
 
-			<?if($image):?>
-			    <a href="<? echo $arItem['DETAIL_PAGE_URL']; ?>" class="image" style="background-image: url(<?=$image['src'];?>)" title="<?=$imgTitle;?>"></a>
+            <?if($image):?>
+                <a href="<? echo $arItem['DETAIL_PAGE_URL']; ?>" class="image" style="background-image: url(<?=$image['src'];?>)" title="<?=$imgTitle;?>"></a>
             <?else:?>
                 <a href="<? echo $arItem['DETAIL_PAGE_URL']; ?>" class="image" title="<?=$imgTitle;?>"></a>
-			<?endif;?>
+            <?endif;?>
 
-			<a href="<? echo $arItem['DETAIL_PAGE_URL']; ?>" class="catalog-name"><? echo TruncateText($arItem['NAME'], 30); ?></a>
+            <a href="<? echo $arItem['DETAIL_PAGE_URL']; ?>" class="catalog-name"><? echo TruncateText($arItem['NAME'], 30); ?></a>
 
-			<div class="price-block">
-				<?if (!empty($arItem['MIN_PRICE']) && (!isset($arItem['OFFERS']) || empty($arItem['OFFERS']))):?>
-				<?=$CSiteController->getHtmlFormatedPrice($arItem["MIN_PRICE"]["CURRENCY"], $arItem["MIN_PRICE"]["DISCOUNT_VALUE"]);?>
-				<?endif;?>
-			</div>
+            <div class="price-block">
+                <?if (!empty($arItem['MIN_PRICE']) && (!isset($arItem['OFFERS']) || empty($arItem['OFFERS']))):?>
+                <?=$CSiteController->getHtmlFormatedPrice($arItem["MIN_PRICE"]["CURRENCY"], $arItem["MIN_PRICE"]["DISCOUNT_VALUE"]);?>
+                <?endif;?>
+            </div>
 
-			<?
-			if ($arItem['CAN_BUY'])
-			{
-				#BUY_URL
-				?>
-				<a class="buy button round green" data-action="add2basket" href="<?=$arItem['ADD_URL']; ?>" rel="nofollow">Купить</a>
-			<?
-			}
-			else
-			{
-				?>
-				<a class="buy button round white not_av" href="javascript:void(0);" rel="nofollow">Купить</a>
-			<?
-			}
-			?>
+            <?
+            if ($arItem['CAN_BUY'])
+            {
+                #BUY_URL
+                ?>
+                <a class="buy button round green" data-action="add2basket" href="<?=$arItem['ADD_URL']; ?>" rel="nofollow">Купить</a>
+            <?
+            }
+            else
+            {
+                ?>
+                <a class="buy button round white not_av" href="javascript:void(0);" rel="nofollow">Купить</a>
+            <?
+            }
+            ?>
 
 
-		</li><?
-	}
-	?></ul><?
+        </li><?
+    }
+    ?></ul><?
 
-	if ($arParams["DISPLAY_BOTTOM_PAGER"])
-		echo $arResult["NAV_STRING"];
+    if ($arParams["DISPLAY_BOTTOM_PAGER"])
+        echo $arResult["NAV_STRING"];
 
 }
 ?>
